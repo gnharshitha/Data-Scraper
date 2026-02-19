73d06fd50bb2c49fe2073a12fcbe64e64b8e54b2
Harshitha G N <gnharshitha8@gmail.com>
2026-02-19 15:53:28 +0530

    Add files via upload
    Added file structure for the project

---
+++ ScrapeX/src/main/java/com/Scraper/ScrapeX/ScrapeXApplication.java
@@ -0,0 +1,13 @@
+package com.Scraper.ScrapeX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapeXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapeXApplication.class, args);
+	}
+
+}
---
+++ ScrapeX/src/test/java/com/Scraper/ScrapeX/ScrapeXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Scraper.ScrapeX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapeXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
