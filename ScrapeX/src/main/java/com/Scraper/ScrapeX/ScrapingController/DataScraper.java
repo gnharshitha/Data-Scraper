92641341d5e232df62aa3c095e3d1178a6cff72a
Harshitha G N <gnharshitha8@gmail.com>
2026-02-20 17:21:21 +0530

    Initial basic code added

--- ScrapeX/src/main/java/com/Scraper/ScrapeX/ScrapingController/DataScraper.java
+++ ScrapeX/src/main/java/com/Scraper/ScrapeX/ScrapingController/DataScraper.java
@@ -1,7 +1,24 @@
 @RestController
 public class DataScraper {
-  
-public void Link()
 
+    @Autowired
+    ScrapeService service;
+  
+ @PostMapping("/url") 
+public void readIN(@RequestBody String URL,String user,String password)
+
+ boolean login;
+ service.Scraper(URL);
+ System.out.println("login Successful");
+ 
+
+
+ if(login){
+    
+        
+ }
+ else{
+     
+ }
 
 }
---
+++ ScrapeX/src/main/java/com/Scraper/ScrapeX/Service/ScrapeService.java
@@ -0,0 +1,13 @@
+public class ScrapeService
+{
+    public void Scraper(){
+        
+   WebDriver driver = new ChromeDriver();
+   driver.get(redirectUrl);
+   String username;
+   String password;
+  if(login){
+       
+  }
+    }
+}
